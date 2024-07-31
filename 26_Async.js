6dfda9f3f5173019e182de108a582cbd11a307c2
tnsqqq <tnsqqqbaliyan@gmail.com>
2024-07-31 21:49:41 +0530

    fourth

---
+++ 26_Async.js
@@ -0,0 +1,32 @@
+// Javascript
+
+// 1. Synchronous --> one by one code execution
+// 2. Single threaded
+
+
+// Execution Context
+
+// execute one line of code at a time
+
+// console log -> 1
+// console log -> 2
+
+
+// each operation waits for the last one to complete before execution
+
+
+
+
+
+
+
+
+
+
+// Blocking Code                                  Non Blocking Code
+
+// 1. Block the flow of program                   1. Does not block execution
+// 2. read file sync                              2. read file sync
+
+
+
