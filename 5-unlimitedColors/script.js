fd6096148d55cf4b4ee45194ea94bbe585f58167
tnsqqq <tnsqqqbaliyan@gmail.com>
2024-08-02 20:22:32 +0530

    fifth

---
+++ 5-unlimitedColors/script.js
@@ -0,0 +1,28 @@
+// genrate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF"
+  let color = "#"
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let interval;
+const startChangingColor = function () {
+  if (!interval) {
+    interval = setInterval(change, 1000)
+  }
+  function change() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(interval);
+  interval = null;
+};
+
+document.querySelector('.start').addEventListener('click', startChangingColor)
+
+document.querySelector('.stop').addEventListener('click', stopChangingColor)
